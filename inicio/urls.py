03324ea8afc07df75f3c29efef4bf148afe5b1ab
Jpptauri <paulino@taurizano.com>
2024-10-15 20:17:57 -0300

    agregue mas templates

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from inicio.views import mi_vista, inicio, vista_datos1
+from inicio.views import mi_vista, inicio, vista_datos1, primer_template,segundo_template
 
 urlpatterns = [
     path('mi-vista/', mi_vista),
     path('', inicio),
     path('vista-datos1/', vista_datos1),
-    path('vista-datos1/<nombre>/', vista_datos1)
+    path('vista-datos1/<nombre>/', vista_datos1),
+    path('primer_template/', primer_template),
+    path('segundo_template/', segundo_template)
 
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+from django.template import Template, Context, loader
+from datetime import datetime
+from django.shortcuts import render
 def mi_vista(request):
     return HttpResponse('mi vista')
 
@@ -9,3 +11,32 @@ def inicio(request):
 def vista_datos1(request,nombre):
     nombre_en_mayuscula = nombre.upper()
     return HttpResponse(f'hola {nombre_en_mayuscula}!!')
+
+def primer_template(request):
+    archivo = open(r'templates\primer_template.html')
+    template = Template(archivo.read())
+    contexto = Context()
+    render_t = template.render(contexto)
+    archivo.close()
+    return(HttpResponse(render_t))
+
+def segundo_template(request):
+    fecha_actual = datetime.now()
+    datos = {
+        'fecha_actual':fecha_actual,
+        'numeros':list(range(1,11))
+    }
+    
+    #V1 
+    #with open(r'templates\segundo_template.html') as archivo:
+    #    template = Template(archivo.read())
+    #contexto = Context(datos)
+    #render_t = template.render(contexto)
+    
+    #V2
+    #template = loader.get_template('segundo_template.html')
+    #render_t = template.render(datos)
+    #return(HttpResponse(render_t))
+    
+    #V3
+    return render(request,'segundo_template.html',datos)
